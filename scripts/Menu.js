1ed55f11843869d930aac853c7de8573abb95147
Brandon G. Elzy <brandon.g.elzy@gmail.com>
2018-03-13 12:37:48 -0500

    Added Terminal icons for some technologies. Must put link somewhere on page in order to use the icons.

--- scripts/Menu.js
+++ scripts/Menu.js
@@ -8,19 +8,23 @@
 
   // Show menu when menu button is clicked.
   OUT_MENU_BTN_EL.addEventListener('click',()=>{
-    MENU_CTR_EL.style.left = '0px';
-    COVER_EL.style.visibility = 'visible';
+    MENU_CTR_EL.style.transform = 'translateX(0px)';
+    COVER_EL.style.pointerEvents = 'auto';
+    COVER_EL.style.opacity = '1';
+
   });
 
   // Hide menu when menu button is clicked.
   IN_MENU_BTN_EL.addEventListener('click',()=>{
-    MENU_CTR_EL.style.left = '-253px';
-    COVER_EL.style.visibility = 'hidden';
+    MENU_CTR_EL.style.transform = 'translateX(-102%)';
+    COVER_EL.style.pointerEvents = 'none';
+    COVER_EL.style.opacity = '0';
   });
 
   // Hide menu when user clicks outside of menu.
   COVER_EL.addEventListener('click', ()=>{
-    MENU_CTR_EL.style.left = '-253px';
-    COVER_EL.style.visibility = 'hidden';
+    MENU_CTR_EL.style.transform = 'translateX(-102%)';
+    COVER_EL.style.pointerEvents = 'none';
+    COVER_EL.style.opacity = '0';
   });
 })();
